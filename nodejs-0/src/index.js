9118890e15ea7effd1968c20428af8caa78a9cf6
matheusads <matheus_ads@hotmail.com>
2020-06-06 15:12:12 -0300

    resolved fibo

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,8 +1,24 @@
 'use strict'
 
-const fibonacci = () => null
+const fibonacci = () => {
+   
+    const maxValue = 350, fiboArray = [0, 1];
+    let number = 1;
+    while (number <= maxValue) {
+        number = fiboArray[fiboArray.length - 1] + fiboArray[fiboArray.length - 2]
+        fiboArray.push(number);
+    }
+    return fiboArray;
 
-const isFibonnaci = (num) => null
+}
+
+const isPerfectSquare = (n) => {
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+}
+
+const isFibonnaci = (n) => {
+    return isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n - 4); 
+}
 
 module.exports = {
     fibonacci,
